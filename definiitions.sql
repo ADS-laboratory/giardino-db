a17e1d347fe01756b321fd3403033c786076df5d
Leonardo Danelutti <leodane2002@gmail.com>
2023-11-30 19:28:51 +0100

    Fixed commas and more compact syntax

--- definiitions.sql
+++ definiitions.sql
@@ -14,48 +14,49 @@ CREATE TABLE Giardino.Famiglia (
 
 CREATE TABLE Giardino.Genere (
     nome varchar(50) PRIMARY KEY,
-    famiglia varchar(50) REFERENCES Giardino.Famiglia(nome) NOT NULL,
+    famiglia varchar(50) REFERENCES Giardino.Famiglia NOT NULL
 );
 
 CREATE TABLE Giardino.SensibileAlClima (
-    famiglia varchar(50) PRIMARY KEY REFERENCES Giardino.Famiglia(nome),
+    famiglia varchar(50) PRIMARY KEY REFERENCES Giardino.Famiglia
 );
 
 CREATE TABLE Giardino.Clima (
-    nome varchar(50) PRIMARY KEY,
+    nome varchar(50) PRIMARY KEY
 );
 
 CREATE TABLE Giardino.PuòStare (
-    clima varchar(50) REFERENCES Giardino.Clima(nome),
-    sensibile_al_clima varchar(50) REFERENCES Giardino.SensibileAlClima(famiglia),
+    clima varchar(50) REFERENCES Giardino.Clima,
+    sensibile_al_clima varchar(50) REFERENCES Giardino.SensibileAlClima,
     PRIMARY KEY (clima, sensibile_al_clima)
 );
 
 CREATE TABLE Giardino.Posizione (
     codice char(5) PRIMARY KEY,
-    clima varchar(50) REFERENCES Giardino.Clima(nome) NOT NULL,
+    clima varchar(50) REFERENCES Giardino.Clima NOT NULL,
     nome varchar(50),
-    coordinate Giardino.COORDINATE UNIQUE,
+    coordinate Giardino.COORDINATE UNIQUE
 );
 
 CREATE TABLE Giardino.Pianta (
     numero integer,
-    genere varchar(50) REFERENCES Giardino.Genere(nome) NOT NULL,
-    posizione char(5) REFERENCES Giardino.Posizione(codice),
+    genere varchar(50) REFERENCES Giardino.Genere NOT NULL,
+    posizione char(5) REFERENCES Giardino.Posizione,
     PRIMARY KEY (numero, genere)
 );
 
 CREATE TABLE Giardino.Giardiniere (
     cf char(16) PRIMARY KEY,
     nome varchar(50) NOT NULL,
-    cognome varchar(50) NOT NULL,
+    cognome varchar(50) NOT NULL
 );
 
 CREATE TABLE Giardino.ÈResponsabile (
-    numero_pianta integer REFERENCES Giardino.Pianta(numero),
-    genere_pianta varchar(50) REFERENCES Giardino.Pianta(genere),
+    numero_pianta integer,
+    genere_pianta varchar(50),
     giardiniere char(16) REFERENCES Giardino.Giardiniere(cf),
-    PRIMARY KEY (numero_pianta, genere_pianta, giardiniere)
+    PRIMARY KEY (numero_pianta, genere_pianta, giardiniere),
+    FOREIGN KEY (numero_pianta, genere_pianta) REFERENCES Giardino.Pianta
 );  
 
 CREATE TABLE Giardino.Orario (
@@ -67,10 +68,9 @@ CREATE TABLE Giardino.Orario (
 
 CREATE TABLE Giardino.Lavora (
     giardiniere char(16) REFERENCES Giardino.Giardiniere(cf),
-    giorno_della_settimana smallint NOT NULL,
-    ora_inizio time NOT NULL,
-    ora_fine time NOT NULL,
-    PRIMARY KEY (giardiniere, giorno_della_settimana, ora_inizio, ora_fine)
-    FOREIGN KEY (giorno_della_settimana, ora_inizio, ora_fine) 
-        REFERENCES Giardino.Orario(giorno_della_settimana, ora_inizio, ora_fine)
+    giorno_della_settimana smallint,
+    ora_inizio time,
+    ora_fine time,
+    PRIMARY KEY (giardiniere, giorno_della_settimana, ora_inizio, ora_fine),
+    FOREIGN KEY (giorno_della_settimana, ora_inizio, ora_fine) REFERENCES Giardino.Orario
 );
