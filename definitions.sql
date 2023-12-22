e5ebae83b49113da901a15b53ac6ad64f5e24392
Leonardo Danelutti <leodane2002@gmail.com>
2023-12-22 11:14:07 +0100

    Fiexed errors and added set search_path

--- definitions.sql
+++ definitions.sql
@@ -1,115 +1,117 @@
 
 CREATE schema Giardino;
 
+SET search_path TO Giardino;
+
 -- DEFINIZIONE DEI DOMINI
 
 -- Definizione del dominio COORDINATE
-CREATE domain Giardino.COORDINATE AS REAL[2] 
+CREATE domain COORDINATE AS REAL[2] 
     CONSTRAINT latitudine CHECK (value[0] >= -90 and value[0] <= 90)
     CONSTRAINT longitudine CHECK (value[1] >= -180 and value[1] <= 180)
     CONSTRAINT not_null CHECK (value[0] IS NOT NULL AND value[1] IS NOT NULL);
 
 -- DEFINIZIONE DELLE TABELLE
 
-CREATE TABLE Giardino.Famiglia (
+CREATE TABLE Famiglia (
     nome varchar(50) PRIMARY KEY,
     descrizione varchar(1000)
 );
 
-CREATE TABLE Giardino.Genere (
+CREATE TABLE Genere (
     nome varchar(50) PRIMARY KEY,
-    famiglia varchar(50) REFERENCES Giardino.Famiglia
-        NOT NULL
+    famiglia varchar(50) REFERENCES Famiglia
         ON DELETE RESTRICT
         ON UPDATE CASCADE
+        NOT NULL,
     max_id integer NOT NULL
 );
 
-CREATE TABLE Giardino.SensibileAlClima (
-    famiglia varchar(50) PRIMARY KEY REFERENCES Giardino.Famiglia
+CREATE TABLE SensibileAlClima (
+    famiglia varchar(50) PRIMARY KEY REFERENCES Famiglia
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
-CREATE TABLE Giardino.Clima (
+CREATE TABLE Clima (
     nome varchar(50) PRIMARY KEY
 );
 
-CREATE TABLE Giardino.PuoStare (
-    clima varchar(50) REFERENCES Giardino.Clima
+CREATE TABLE PuoStare (
+    clima varchar(50) REFERENCES Clima
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    sensibile_al_clima varchar(50) REFERENCES Giardino.SensibileAlClima
+    sensibile_al_clima varchar(50) REFERENCES SensibileAlClima
         ON DELETE CASCADE -- Aggiungere trigger per controllare che ci siana almeno un elemento
         ON UPDATE CASCADE,
     PRIMARY KEY (clima, sensibile_al_clima)
 );
 
-CREATE TABLE Giardino.Posizione (
+CREATE TABLE Posizione (
     codice char(5) PRIMARY KEY,
-    clima varchar(50) REFERENCES Giardino.Clima 
-        NOT NULL
-        ON DELETE RESTRICT,
-        ON UPDATE CASCADE,
+    clima varchar(50) REFERENCES Clima 
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE
+        NOT NULL,
     nome varchar(50),
-    coordinate Giardino.COORDINATE UNIQUE
+    coordinate COORDINATE UNIQUE
 );
 
-CREATE TABLE Giardino.Pianta (
+CREATE TABLE Pianta (
     numero integer,
-    genere varchar(50) REFERENCES Giardino.Genere 
-        NOT NULL
+    genere varchar(50) REFERENCES Genere 
         ON DELETE RESTRICT
-        ON UPDATE CASCADE,
-    posizione char(5) REFERENCES Giardino.Posizione
+        ON UPDATE CASCADE
+        NOT NULL,
+    posizione char(5) REFERENCES Posizione
         ON DELETE RESTRICT
         ON UPDATE CASCADE,
     PRIMARY KEY (numero, genere)
 );
 
-CREATE TABLE Giardino.Giardiniere (
+CREATE TABLE Giardiniere (
     cf char(16) PRIMARY KEY,
     nome varchar(50) NOT NULL,
     cognome varchar(50) NOT NULL
 );
 
-CREATE TABLE Giardino.EResponsabile (
+CREATE TABLE EResponsabile (
     numero_pianta integer,
     genere_pianta varchar(50),
-    giardiniere char(16) REFERENCES Giardino.Giardiniere(cf)
+    giardiniere char(16) REFERENCES Giardiniere(cf)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     PRIMARY KEY (numero_pianta, genere_pianta, giardiniere),
-    FOREIGN KEY (numero_pianta, genere_pianta) REFERENCES Giardino.Pianta
+    FOREIGN KEY (numero_pianta, genere_pianta) REFERENCES Pianta
         ON DELETE CASCADE
         ON UPDATE CASCADE
-);  
+);
 
-CREATE TABLE Giardino.Orario (
+CREATE TABLE Orario (
     giorno_della_settimana smallint,
     ora_inizio time,
     ora_fine time,
     PRIMARY KEY (giorno_della_settimana, ora_inizio, ora_fine)
 );
 
-CREATE TABLE Giardino.Lavora (
-    giardiniere char(16) REFERENCES Giardino.Giardiniere(cf),
+CREATE TABLE Lavora (
+    giardiniere char(16) REFERENCES Giardiniere(cf)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     giorno_della_settimana smallint,
     ora_inizio time,
     ora_fine time,
     PRIMARY KEY (giardiniere, giorno_della_settimana, ora_inizio, ora_fine),
-    FOREIGN KEY (giorno_della_settimana, ora_inizio, ora_fine) REFERENCES Giardino.Orario
+    FOREIGN KEY (giorno_della_settimana, ora_inizio, ora_fine) REFERENCES Orario
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
-CREATE TABLE Giardino.GP (
-    genere varchar(50) REFERENCES Giardino.Genere
+CREATE TABLE GP (
+    genere varchar(50) REFERENCES Genere
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    posizione char(5) REFERENCES Giardino.Posizione
+    posizione char(5) REFERENCES Posizione
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     PRIMARY KEY (genere, posizione)
@@ -118,15 +120,15 @@ CREATE TABLE Giardino.GP (
 -- DEFINIZIONE DELLE FUNZIONI E DEI TRIGGER
 
 -- controllo che un giardiniere abbia almeno un orario
-CREATE OR REPLACE FUNCTION Giardino.check_orario_giardiniere()
-RETURNS TRIGGER LENGUAGE plpgsql AS 
+CREATE OR REPLACE FUNCTION check_orario_giardiniere()
+RETURNS TRIGGER LANGUAGE plpgsql AS 
 $$
     DECLARE
         count integer;
     BEGIN
         SELECT COUNT(*) INTO count
-        FROM Giardino.Lavora
-        WHERE OLD.giardiniere = giardiniere
+        FROM Lavora
+        WHERE OLD.giardiniere = giardiniere;
         IF count <= 1 THEN
             RAISE EXCEPTION 'Un giardiniere deve avere almeno un orario';
         END IF;
@@ -136,20 +138,20 @@ $$;
 
 -- controllo che un giardiniere abbia almeno un orario
 CREATE TRIGGER check_orario_giardiniere
-BEFORE DELETE ON Giardino.Lavora
+BEFORE DELETE ON Lavora
 FOR EACH ROW
-EXECUTE PROCEDURE Giardino.check_orario_giardiniere();
+EXECUTE PROCEDURE check_orario_giardiniere();
 
 -- controllo che una famiglia sensibile al clima abbia almeno un clima a cui è sensibile
-CREATE OR REPLACE FUNCTION Giardino.check_sensibile_al_clima()
-RETURNS TRIGGER LENGUAGE plpgsql AS
+CREATE OR REPLACE FUNCTION check_sensibile_al_clima()
+RETURNS TRIGGER LANGUAGE plpgsql AS
 $$
     DECLARE
         count integer;
     BEGIN
         SELECT COUNT(*) INTO count
-        FROM Giardino.PuoStare
-        WHERE OLD.famiglia = sensibile_al_clima
+        FROM PuoStare
+        WHERE OLD.famiglia = sensibile_al_clima;
         IF count <= 1 THEN
             RAISE EXCEPTION 'Una famiglia sensibile al clima deve avere almeno un clima a cui è sensibile';
         END IF;
@@ -159,8 +161,8 @@ $$;
 
 -- controllo che una famiglia sensibile al clima abbia almeno un clima a cui è sensibile
 CREATE TRIGGER check_sensibile_al_clima
-BEFORE DELETE ON Giardino.PuoStare
+BEFORE DELETE ON PuoStare
 FOR EACH ROW
-EXECUTE PROCEDURE Giardino.check_sensibile_al_clima();
+EXECUTE PROCEDURE check_sensibile_al_clima();
 
 -- Aggiornamento dell'attributo max_id di Genere
