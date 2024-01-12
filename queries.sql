d57712bfc283ef8eebbef79cd5978423713ed676
francesco <fbortuzzo02@gmail.com>
2024-01-12 10:34:32 +0100

    Added new query

--- queries.sql
+++ queries.sql
@@ -5,3 +5,19 @@ FROM Giardino.Pianta Pianta
     JOIN Giardino.EResponsabile EResponsabile ON EResponsabile.Numero_Pianta = Pianta.Numero AND EResponsabile.Genere_Pianta = Pianta.Genere
     JOIN Giardino.Lavora Lavora ON Lavora.Giardiniere = EResponsabile.Giardiniere
     WHERE Lavora.Ora_Inizio >= '08:00:00' AND Lavora.Ora_Fine <= '17:00:00';
+
+
+-- Trovare la Posizione coperta da meno Giardinieri
+
+CREATE VIEW Giardino.Numero_Giardinieri AS V1(
+    SELECT Posizione, COUNT(*) AS Numero_Giardinieri
+    FROM Giardino.Posizione as Posizione
+        JOIN Giardino.Pianta as Pianta ON Pianta.posizione = Posizione.codice
+        JOIN Giardino.EResponsabile as EResponsabile ON EResponsabile.Genere_Pianta = GP.Genere
+    GROUP BY Posizione
+)
+
+SELECT Posizione, Numero_Giardinieri
+FROM V1
+WHERE Numero_Giardinieri <= ALL(SELECT Numero_Giardinieri FROM V1)
+ORDER BY Posizione;
