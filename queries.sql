e1c8731ce6b6a987e7627aa56a59be15ae679e9a
francesco <fbortuzzo02@gmail.com>
2024-01-18 11:35:35 +0100

    fixed query 6, 8

--- queries.sql
+++ queries.sql
@@ -107,7 +107,7 @@ $$;
 
 
 -- Operazione 5
--- Data una Posizione trovare i Generi che possono stare solo lì
+-- Trovare il Genere di Piante che si trova in meno Posizioni
 
 CREATE OR REPLACE FUNCTION genera_meno_posizione(
 )
@@ -130,17 +130,25 @@ $$;
 -- Operazione 6
 -- Trovare la Posizione coperta da meno Giardinieri
 
-CREATE VIEW V1 AS
-    SELECT Posizione, COUNT(DISTINCT EResponsabile.giardiniere) AS Numero_Giardinieri
-    FROM Posizione
-        JOIN Pianta ON Pianta.posizione = Posizione.codice
-        JOIN EResponsabile ON EResponsabile.genere_pianta = Pianta.genere AND EResponsabile.numero_pianta = Pianta.numero
-    GROUP BY Posizione;
+CREATE OR REPLACE FUNCTION posizione_con_meno_giardinieri()
+RETURNS TABLE (posizione char(5), numero_Giardinieri integer) LANGUAGE plpgsql AS
+$$
+    BEGIN
+        RETURN QUERY
+        -- Il numero di giardinieri per ogni posizione
+        CREATE VIEW Giardinieri_Per_Posizione AS
+            SELECT Posizione, COUNT(DISTINCT EResponsabile.giardiniere) AS numero_Giardinieri
+            FROM Posizione
+                JOIN Pianta ON Pianta.posizione = Posizione.codice
+                JOIN EResponsabile ON EResponsabile.genere_pianta = Pianta.genere AND EResponsabile.numero_pianta = Pianta.numero
+            GROUP BY Posizione;
 
-SELECT *
-FROM V1
-WHERE Numero_Giardinieri <= ALL(SELECT Numero_Giardinieri FROM V1)
-ORDER BY Posizione;
+        SELECT *
+        FROM Giardinieri_Per_Posizione
+        WHERE numero_Giardinieri = (SELECT MIN(numero_Giardinieri) FROM Giardinieri_Per_Posizione);
+        ORDER BY Posizione;
+    END;
+$$;
 
 
 -- Operazione 7
@@ -155,33 +163,41 @@ FROM Pianta
 -- Operazione 8
 -- Il Clima delle Posizioni in cui si trovano almeno 10 Piante del Genere x e almeno 20 del Genere y
 
--- Il numero di piante del genere x in ogni posizione
-CREATE or REPLACE VIEW Numero_Piante_X AS
-    SELECT Posizione, COUNT(*) AS Numero_Piante
-    FROM Posizione
-        JOIN Pianta ON Pianta.posizione = Posizione.codice
-    WHERE Genere = 'Alplily'
-    GROUP BY Posizione, Genere;
+CREATE OR REPLACE FUNCTION genera_meno_posizione(
+    genere_x varchar(50),
+    genere_y varchar(50)
+)
+RETURNS TABLE (clima varchar(50), codice char(5)) LANGUAGE plpgsql AS
+$$
+    BEGIN
+        RETURN QUERY
+         -- Il numero di piante del genere x in ogni posizione
+        CREATE or REPLACE VIEW Numero_Piante_X AS
+            SELECT Posizione, COUNT(*) AS Numero_Piante
+            FROM Posizione
+                JOIN Pianta ON Pianta.posizione = Posizione.codice
+            WHERE Genere = genere_x
+            GROUP BY Posizione, Genere;
 
--- Il numero di piante del genere y in ogni posizione
-CREATE OR REPLACE VIEW Numero_Piante_Y AS
-    SELECT Posizione, COUNT(*) AS Numero_Piante
-    FROM Posizione
-        JOIN Pianta ON Pianta.posizione = Posizione.codice
-    WHERE Genere = 'False Flax'
-    GROUP BY Posizione, Genere;
+        -- Il numero di piante del genere y in ogni posizione
+        CREATE OR REPLACE VIEW Numero_Piante_Y AS
+            SELECT Posizione, COUNT(*) AS Numero_Piante
+            FROM Posizione
+                JOIN Pianta ON Pianta.posizione = Posizione.codice
+            WHERE Genere = genere_y
+            GROUP BY Posizione, Genere;
 
 
-SELECT Clima, codice
-FROM Posizione
-WHERE codice IN (
-    SELECT Numero_Piante_X.Posizione
-    FROM Numero_Piante_X
-    WHERE Numero_Piante_X.Numero_Piante >= 10
-    INTERSECT
-    SELECT Numero_Piante_Y.Posizione
-    FROM Numero_Piante_Y
-    WHERE Numero_Piante_Y.Numero_Piante >= 20
-);
-
--- TODO: Rivedere operazione 8
+        SELECT Clima, codice
+        FROM Posizione
+        WHERE codice IN (
+            SELECT Numero_Piante_X.Posizione
+            FROM Numero_Piante_X
+            WHERE Numero_Piante_X.Numero_Piante >= 10
+            INTERSECT
+            SELECT Numero_Piante_Y.Posizione
+            FROM Numero_Piante_Y
+            WHERE Numero_Piante_Y.Numero_Piante >= 20
+        );
+    END;
+$$;
